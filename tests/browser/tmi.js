f68652290449c61e3de4632faaa90bc95c1978e3
Schmoopiie <Schmoopiie@gmail.com>
2015-06-25 10:43:20 -0400

    Browserified the changes.

--- tests/browser/tmi.js
+++ tests/browser/tmi.js
@@ -1028,12 +1028,142 @@ client.prototype.utils = {
         return new cron.CronJob(time, function () {
             fn();
         }, null, false);
+    },
+    levenshtein: function(s1, s2, caseSensitive) {
+        var cost_ins = 1;
+        var cost_rep = 1;
+        var cost_del = 1;
+        caseSensitive = typeof caseSensitive !== 'undefined' ? caseSensitive : false;
+
+        if (!caseSensitive) {
+            s1 = s1.toLowerCase();
+            s2 = s2.toLowerCase();
+        }
+
+        if (s1 == s2) { return 0; }
+
+        var l1 = s1.length;
+        var l2 = s2.length;
+
+        if (l1 === 0) { return l2 * cost_ins; }
+        if (l2 === 0) { return l1 * cost_del; }
+
+        var split = false;
+        try {
+            split = !('0')[0];
+        } catch (e) {
+            split = true;
+        }
+        if (split) {
+            s1 = s1.split('');
+            s2 = s2.split('');
+        }
+
+        var p1 = new Array(l2 + 1);
+        var p2 = new Array(l2 + 1);
+
+        var i1, i2, c0, c1, c2, tmp;
+
+        for (i2 = 0; i2 <= l2; i2++) {
+            p1[i2] = i2 * cost_ins;
+        }
+
+        for (i1 = 0; i1 < l1; i1++) {
+            p2[0] = p1[0] + cost_del;
+
+            for (i2 = 0; i2 < l2; i2++) {
+                c0 = p1[i2] + ((s1[i1] == s2[i2]) ? 0 : cost_rep);
+                c1 = p1[i2 + 1] + cost_del;
+
+                if (c1 < c0) {
+                    c0 = c1;
+                }
+
+                c2 = p2[i2] + cost_ins;
+
+                if (c2 < c0) {
+                    c0 = c2;
+                }
+
+                p2[i2 + 1] = c0;
+            }
+
+            tmp = p1;
+            p1 = p2;
+            p2 = tmp;
+        }
+
+        c0 = p1[l2];
+
+        return c0;
+    },
+    raffle: {
+        init: function(channel) {
+            if (!this.raffleChannels) { this.raffleChannels = {}; }
+            if (!this.raffleChannels[utils.normalizeChannel(channel)]) { this.raffleChannels[utils.normalizeChannel(channel)] = []; }
+        },
+        enter: function(channel, username) {
+            this.init(channel);
+            this.raffleChannels[channel].push(username.toLowerCase());
+        },
+        leave: function(channel, username) {
+            this.init(channel);
+            var index = this.raffleChannels[utils.normalizeChannel(channel)].indexOf(utils.normalizeUsername(username));
+            if (index >= 0) {
+                this.raffleChannels[utils.normalizeChannel(channel)].splice(index, 1);
+                return true;
+            }
+            return false;
+        },
+        pick: function(channel) {
+            this.init(channel);
+            var count = this.raffleChannels[utils.normalizeChannel(channel)].length;
+            if (count >= 1) {
+                return this.raffleChannels[utils.normalizeChannel(channel)][Math.floor((Math.random() * count))];
+            }
+            return null;
+        },
+        reset: function(channel) {
+            this.init(channel);
+            this.raffleChannels[utils.normalizeChannel(channel)] = [];
+        },
+        count: function(channel) {
+            this.init(channel);
+            if (this.raffleChannels[utils.normalizeChannel(channel)]) {
+                return this.raffleChannels[utils.normalizeChannel(channel)].length;
+            }
+            return 0;
+        },
+        isParticipating: function(channel, username) {
+            this.init(channel);
+            if (this.raffleChannels[utils.normalizeChannel(channel)].indexOf(utils.normalizeUsername(username)) >= 0) {
+                return true;
+            }
+            return false;
+        }
+    },
+    symbols: function(line) {
+        var count = 0;
+        for (var i = 0; i < line.length; i++) {
+            var charCode = line.substring(i, i+1).charCodeAt(0);
+            if ((charCode <= 30 || charCode >= 127) || charCode === 65533) {
+                count++;
+            }
+        }
+        return Math.ceil((count / line.length) * 100) / 100;
+    },
+    uppercase: function(string) {
+        var chars = string.length;
+        var u_let = string.match(/[A-Z]/g);
+        if (u_let !== null) {
+            return (u_let.length / chars);
+        }
+        return 0;
     }
 };
 
 // Expose everything, for browser and Node.js / io.js
 if (typeof window !== "undefined") {
-    window.ignore = null;
     window.irc = {};
     window.irc.client = client;
 } else {
