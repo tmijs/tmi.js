1542c1039a34992adfc90dfbcfe5d6a848461f92
Schmoopiie <Schmoopiie@gmail.com>
2015-08-12 23:20:00 -0400

    tests: updated html and node.js tests.

--- tests/browser/script.js
+++ tests/browser/script.js
@@ -1,128 +1,26 @@
-var options = {
+var clientOptions = {
     options: {
         debug: true
     },
     connection: {
         random: "chat",
-        reconnect: false,
-        server: "192.16.64.145",
-        port: 443
+        reconnect: true
     },
     identity: {
-        username: "justinfan432543",
-        password: "SCHMOOPIIE"
+        username: "Schmoopiie",
+        password: "oauth:3eb787117110834e079932bedfb8e6a7"
     },
     channels: ["#schmoopiie"]
 };
 
-var client = new irc.client(options);
+var client = new irc.client(clientOptions);
 
 client.connect();
-
-client.on("connecting", function(server, port) {
-    //
-});
-
-client.on("logon", function() {
-    //
-});
-
-client.on("connected", function(server, port) {
-    //
-});
-
-client.on("disconnected", function(reason) {
-    //
-});
-
-client.on("names", function(channel, names) {
-    //
-});
 
 client.on("action", function(channel, user, message, self) {
     //
 });
 
 client.on("chat", function(channel, user, message, self) {
-    //
-});
-
-client.on("message", function(channel, user, message, self) {
-    //
-});
-
-client.on("timeout", function(channel, username) {
-    //
-});
-
-client.on("clearchat", function(channel) {
-    //
-});
-
-client.on("hosting", function(channel, target, viewers) {
-    //
-});
-
-client.on("unhost", function(channel) {
-    //
-});
-
-client.on("subscribers", function(channel, enabled) {
-    //
-});
-
-client.on("slow", function(channel, enabled) {
-    //
-});
-
-client.on("r9kmode", function(channel, enabled) {
-    //
-});
-
-client.on("join", function(channel, username) {
-    //
-});
-
-client.on("part", function(channel, username) {
-    //
-});
-
-client.on("roomstate", function(channel, state) {
-    //
-});
-
-client.on("ping", function() {
-    //
-});
-
-client.on("reconnect", function() {
-    //
-});
-
-client.on("mod", function(channel, username) {
-    //
-});
-
-client.on("unmod", function(channel, username) {
-    //
-});
-
-client.on("subscription", function(channel, username) {
-    //
-});
-
-client.on("subanniversary", function(channel, username, length) {
-    //
-});
-
-client.on("mods", function(channel, mods) {
-    //
-});
-
-client.on("hosted", function(channel, username) {
-    //
-});
-
-client.on("whisper", function(username, message) {
     //
 });
