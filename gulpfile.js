0f59d2e648856e04759003104c488ac0104571b6
Schmoopiie <schmoopiie@tmijs.org>
2016-02-06 09:16:35 -0500

    client: updated ws to 1.0.1 and removed the module from bundle. #85

--- gulpfile.js
+++ gulpfile.js
@@ -4,31 +4,36 @@ var source = require("vinyl-source-stream");
 var buffer = require("vinyl-buffer");
 var browserify = require("browserify");
 var uglify = require("gulp-uglify");
-var glob = require("glob");
 var babel = require("babelify");
 var size = require("gulp-size");
 
 function compile(done) {
-    glob("lib/*.js", function(err, files) {
-        if (err) { done(err); }
-        files.push("index.js");
+    function rebundle() {
+        var bundler = browserify(["./index.js"], {
+            debug: true,
+            builtins: true
+        }).transform(babel);
 
-        var bundler = browserify(files, { debug: true }).transform(babel);
+        bundler.exclude("mocha");
+        bundler.exclude("should");
+        bundler.exclude("request");
+        bundler.exclude("locallydb");
+        bundler.exclude("ws");
+        bundler.exclude("utf-8-validate");
+        bundler.exclude("bufferutil");
 
-        function rebundle() {
-            bundler.bundle()
-            .on("error", function(err) { console.error(err); this.emit("end"); })
-            .pipe(source("tmi.js"))
-            .pipe(buffer())
-            .pipe(sourcemaps.init({ loadMaps: true }))
-            .pipe(uglify())
-            .pipe(sourcemaps.write("./"))
-            .pipe(gulp.dest("./dist"))
-            .pipe(size({ showFiles: true }));
-        }
+        bundler.bundle()
+        .on("error", function(err) { console.error(err); this.emit("end"); })
+        .pipe(source("tmi.js"))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest("./dist"))
+        .pipe(size({ showFiles: true }));
+    }
 
-        rebundle();
-    });
+    rebundle();
 }
 
 gulp.task("default", function() { return compile(); });
