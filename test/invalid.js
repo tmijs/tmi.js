f5054e39e4dec29fac95436d4c277cff3e5186d3
Ben Briggs <beneb.info@gmail.com>
2016-04-07 14:27:34 +0100

    tests: Add tests for handling invalid server responses.

---
+++ test/invalid.js
@@ -0,0 +1,25 @@
+var irc = require('../index.js');
+
+var tests = [
+    'FOO',
+    ':tmi.twitch.tv FOO',
+    ':tmi.twitch.tv NOTICE #schmoopiie : FOO',
+    ':jtv FOO',
+    ':schmoopiie!schmoopiie@schmoopiie.tmi.twitch.tv FOO'
+];
+
+describe('invalid server events', function() {
+    tests.forEach(function(test) {
+        it(`should treat "${test}" as invalid`, function() {
+            var client = new irc.client({
+                logger: {
+                    warn: function(message) {
+                        message.includes('Could not parse').should.be.ok;
+                    }
+                }
+            });
+
+            client._onMessage({data: test});
+        });
+    });
+});
