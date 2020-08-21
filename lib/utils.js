tmijs/tmi.js

commit 250445f2d01602c4799dfc411c7e62c89533969a

various: add IRC escape util and tag form function

lib/utils.js
@@ -1,7 +1,9 @@
 var actionMessageRegex = /^\u0001ACTION ([^\u0001]+)\u0001$/;
 var justinFanRegex = /^(justinfan)(\d+$)/;
 var unescapeIRCRegex = /\\([sn:r\\])/g;
-var ircEscapedChars = { s: ' ', n: '', ':': ';', r: '' };
+var escapeIRCRegex = /([ \n;\r\\])/g;
+var ircEscapedChars = { s: ' ', n: '\n', ':': ';', r: '\r' };
+var ircUnescapedChars = { ' ': 's', '\n': 'n', ';': ':', '\r': 'r' };
 var self = module.exports = {
 	// Return the second value if the first value is undefined..
 	get: (obj1, obj2) => typeof obj1 === "undefined" ? obj2 : obj1,
@@ -70,6 +72,12 @@ var self = module.exports = {
 		msg.replace(
 			unescapeIRCRegex,
 			(m, p) => p in ircEscapedChars ? ircEscapedChars[p] : p
+		),
+	
+	escapeIRC: msg => !msg ? msg :
+		msg.replace(
+			escapeIRCRegex,
+			(m, p) => p in ircUnescapedChars ? `\\${ircUnescapedChars[p]}` : p
 		),
 
 	actionMessage: msg => msg.match(actionMessageRegex),

lib/parser.js
@@ -118,6 +118,15 @@ module.exports = {
 		return transformed.slice(0,-1);
 	},
 
+	formTags(tags) {
+		var result = [];
+		for(var key in tags) {
+			var value = _.escapeIRC(tags[key]);
+			result.push(`${key}=${value}`);
+		}
+		return `@${result.join(';')}`;
+	},
+
 	// Parse Twitch messages..
 	msg(data) {
 		var message = {

commit 1b8d6dc2eeca4433e3eeb23f1ccc12d0a2f846e6

utils: fix tag regression from 250445f

lib/utils.js
@@ -2,7 +2,7 @@ var actionMessageRegex = /^\u0001ACTION ([^\u0001]+)\u0001$/;
 var justinFanRegex = /^(justinfan)(\d+$)/;
 var unescapeIRCRegex = /\\([sn:r\\])/g;
 var escapeIRCRegex = /([ \n;\r\\])/g;
-var ircEscapedChars = { s: ' ', n: '\n', ':': ';', r: '\r' };
+var ircEscapedChars = { s: ' ', n: '', ':': ';', r: '' };
 var ircUnescapedChars = { ' ': 's', '\n': 'n', ';': ':', '\r': 'r' };
 var self = module.exports = {
 	// Return the second value if the first value is undefined..