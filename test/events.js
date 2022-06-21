3a1aff1391382d4cc4803b7951a066bec095ca38
Jacob Foster <jfost3@gmail.com>
2022-06-21 19:52:25 +0000

    test: remove newchatter/ritual events

--- test/events.js
+++ test/events.js
@@ -584,69 +584,6 @@ const events = [ {
 		false
 	]
 }, {
-	name: 'newchatter',
-	data: '@badge-info=;badges=;color=;display-name=MurdocTurner;emotes=64138:0-8;flags=;id=00000000-0000-0000-0000-000000000000;login=murdocturner;mod=0;msg-id=ritual;msg-param-ritual-name=new_chatter;room-id=123456789;subscriber=0;system-msg=MurdocTurner\\sis\\snew\\shere.\\sSay\\shello!;tmi-sent-ts=1500000000000;user-id=89983882;user-type= :tmi.twitch.tv USERNOTICE #channel :SeemsGood',
-	expected: [
-		'#channel',
-		'MurdocTurner',
-		{
-			'badge-info': null,
-			badges: null,
-			color: null,
-			'display-name': 'MurdocTurner',
-			emotes: { '64138': [ '0-8' ] },
-			flags: null,
-			id: '00000000-0000-0000-0000-000000000000',
-			login: 'murdocturner',
-			mod: false,
-			'msg-id': 'ritual',
-			'message-type': 'ritual',
-			'msg-param-ritual-name': 'new_chatter',
-			'room-id': '123456789',
-			subscriber: false,
-			'system-msg': 'MurdocTurner is new here. Say hello!',
-			'tmi-sent-ts': '1500000000000',
-			'user-id': '89983882',
-			'user-type': null,
-			'emotes-raw': '64138:0-8',
-			'badge-info-raw': null,
-			'badges-raw': null
-		},
-		'SeemsGood'
-	]
-}, {
-	name: 'ritual',
-	data: '@badge-info=;badges=;color=;display-name=MurdocTurner;emotes=64138:0-8;flags=;id=00000000-0000-0000-0000-000000000000;login=murdocturner;mod=0;msg-id=ritual;msg-param-ritual-name=possibledifferentritual;room-id=123456789;subscriber=0;system-msg=MurdocTurner\\sis\\snew\\shere.\\sSay\\shello!;tmi-sent-ts=1500000000000;user-id=89983882;user-type= :tmi.twitch.tv USERNOTICE #channel :SeemsGood',
-	expected: [
-		'possibledifferentritual',
-		'#channel',
-		'MurdocTurner',
-		{
-			'badge-info': null,
-			badges: null,
-			color: null,
-			'display-name': 'MurdocTurner',
-			emotes: { '64138': [ '0-8' ] },
-			flags: null,
-			id: '00000000-0000-0000-0000-000000000000',
-			login: 'murdocturner',
-			mod: false,
-			'msg-id': 'ritual',
-			'message-type': 'ritual',
-			'msg-param-ritual-name': 'possibledifferentritual',
-			'room-id': '123456789',
-			subscriber: false,
-			'system-msg': 'MurdocTurner is new here. Say hello!',
-			'tmi-sent-ts': '1500000000000',
-			'user-id': '89983882',
-			'user-type': null,
-			'emotes-raw': '64138:0-8',
-			'badge-info-raw': null,
-			'badges-raw': null
-		},
-		'SeemsGood'
-	]
-}, {
 	name: 'redeem',
 	data: '@badge-info=;color=#0FC7D1;custom-reward-id=27c8e486-a386-40cc-9a4b-dbb5cf01e439;display-name=MurdocTurner;emotes=;flags=;id=702b9546-f0fe-41cb-b35b-fba5f8315909;mod=1;room-id=7676884;subscriber=0;tmi-sent-ts=1607582291194;turbo=0;user-id=89983882;user-type=mod :murdocturner!murdocturner@murdocturner.tmi.twitch.tv PRIVMSG #channel :lol',
 	expected: [
