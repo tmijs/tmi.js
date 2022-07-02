535b849f4cab407ca4e3f33f050c7c24b8e02823
Jacob Foster <jfost3@gmail.com>
2022-07-02 17:12:26 +0000

    build: export from index as an IIFE

--- scripts/esbuild.js
+++ scripts/esbuild.js
@@ -1,23 +1,29 @@
 const esbuild = require('esbuild');
 
+/** @type {esbuild.BuildOptions} */
 const settingsBase = {
 	entryPoints: [ 'index.js' ],
 	bundle: true,
 	outdir: 'dist',
 	entryNames: 'tmi',
 	target: 'es2018',
+	format: 'iife',
+	globalName: 'tmi',
 };
 
+/** @type {esbuild.BuildOptions} */
 const settingsMinfiy = {
 	minify: true,
 	sourcemap: true,
 	sourcesContent: false,
 	entryNames: 'tmi.min',
+	keepNames: true,
 };
 
 const args = process.argv.slice(2);
 
-const settings =  args[0] === 'minify' ?
+/** @type {esbuild.BuildOptions} */
+const settings = args[0] === 'minify' ?
 	{ ...settingsBase, ...settingsMinfiy } :
 	settingsBase;
 
